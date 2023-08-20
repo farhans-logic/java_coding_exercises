0d61adcaed5ef6eb003028cd876b5df04cf32a4c
Farhan <farhans-logic@users.noreply.github.com>
2023-08-20 19:54:20 +0100

    Add ice cream flavour array and  method implementations for getIceCreamCode and iceCreamFlavours

--- src/main/java/com/techreturners/exercise003/Exercise003.java
+++ src/main/java/com/techreturners/exercise003/Exercise003.java
@@ -1,13 +1,24 @@
 package com.techreturners.exercise003;
 
+import java.util.Arrays;
+
 public class Exercise003 {
 
+    private final String[] iceCreamFlavours = {
+            "Pistachio",
+            "Raspberry Ripple",
+            "Vanilla",
+            "Mint Chocolate Chip",
+            "Chocolate",
+            "Mango Sorbet"
+    };
+
     int getIceCreamCode(String iceCreamFlavour) {
-        throw new UnsupportedOperationException(("You can delete this statement and add your code here."));
+        return Arrays.asList(iceCreamFlavours).indexOf(iceCreamFlavour);
     }
 
     String[] iceCreamFlavours() {
-        throw new UnsupportedOperationException(("You can delete this statement and add your code here."));
+        return iceCreamFlavours;
     }
 
 }
