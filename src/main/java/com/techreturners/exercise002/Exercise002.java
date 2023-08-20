8676e600c79ef4412a407f0321bd509c164d4686
Farhan <farhans-logic@users.noreply.github.com>
2023-08-20 19:48:15 +0100

    Add isFromManchester and CanWatchFilm method implementation

--- src/main/java/com/techreturners/exercise002/Exercise002.java
+++ src/main/java/com/techreturners/exercise002/Exercise002.java
@@ -2,14 +2,13 @@ package com.techreturners.exercise002;
 
 public class Exercise002 {
 
-    public boolean isFromManchester(/* Person person */) {
-        // Add your code here
-        return false;
+    public boolean isFromManchester(Person person) {
+        final String MANCHESTER = "Manchester";
+        return person.getCity().equals(MANCHESTER);
     }
 
-    public boolean canWatchFilm(/* Person person, int ageLimit*/) {
-        // Add your code here
-        return false;
+    public boolean canWatchFilm(Person person, int ageLimit) {
+        return person.getAge() >= ageLimit;
     }
     
 }
