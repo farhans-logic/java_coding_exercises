96f2355129cb263dc97fa8b4a379800d36ff8729
Farhan <farhans-logic@users.noreply.github.com>
2023-08-20 20:03:19 +0100

    Add isPanagram method logic

--- src/main/java/com/techreturners/exercise005/Exercise005.java
+++ src/main/java/com/techreturners/exercise005/Exercise005.java
@@ -1,9 +1,26 @@
 package com.techreturners.exercise005;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Exercise005 {
 
     public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
+        String lowerCaseInput = input.toLowerCase();
+
+        Stream<Character> charStream = lowerCaseInput.chars()
+                .filter(item -> ((item >= 'a' && item <= 'z')))
+                .mapToObj(c -> (char) c);
+
+        Map<Character, Boolean> alphabetMap =
+                charStream.collect(
+                        Collectors.toMap(
+                                item -> item,
+                                k -> Boolean.TRUE,
+                                (p1, p2) -> p1));
+
+        return alphabetMap.size() == 26;
     }
 
 }
